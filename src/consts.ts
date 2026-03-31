7078665fc394de86ad5cd09ff2ac26184ec31b3c
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-30 23:11:47 -0500

    add index content, icon path, fix default accorion icons, remove old index

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const SITE_TITLE = "NitroCSS";
 export const SITE_DESCRIPTION =
   "A CSS Framework for the convience of non-UX Engineers by a UX Engineer";
+export const SVG_PATH = "/img/core-icons.svg";
+export const ICONIC_PATH = "/img/open-iconic-sprites.svg";
