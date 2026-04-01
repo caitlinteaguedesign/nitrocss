a7934f58db4e8e89c519bfd4d683e8fc9cbb8d48
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-31 21:44:29 -0500

    refactor sidebar for DRY, add UX improvement

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,11 @@ export const SITE_DESCRIPTION =
   "A CSS Framework for the convience of non-UX Engineers by a UX Engineer";
 export const SVG_PATH = "/img/core-icons.svg";
 export const ICONIC_PATH = "/img/open-iconic-sprites.svg";
+
+export const SIDEBAR_SECTIONS = {
+  INTRO: "Introduction",
+  LAYOUT: "Layout",
+  CONTENT: "Content",
+  COMPONENTS: "Components",
+  FEATURES: "Interactive Features",
+};
