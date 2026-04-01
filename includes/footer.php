33d00bce20d202b95d5bbda8a4a9ef3c54d04994
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-31 22:17:36 -0500

    convert php files to astro batch #2

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>By Caitlin Doerr, developed for the domestication of css in Leerburg projects for Front-end Developers</footer>
-
-<!-- scroll to top -->
-<button type="button" id="scroll-to-top" class="scroll-to-top scroll-to-top_style-default">Scroll to Top</button>
- 
-</body>
-</html>
