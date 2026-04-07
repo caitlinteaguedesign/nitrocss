d9a98b997aefa75a60773ab6a9e7488acb45f41a
Caitlin Teague Doerr <design@caitlinteague.com>
2026-04-06 22:43:01 -0500

    fix dist paths

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,10 @@
+const baseUrl = import.meta.env.BASE_URL;
+
 export const SITE_TITLE = "NitroCSS";
 export const SITE_DESCRIPTION =
   "A CSS Framework for the convience of non-UX Engineers by a UX Engineer";
-export const SVG_PATH = "/img/core-icons.svg";
-export const ICONIC_PATH = "/img/open-iconic-sprites.svg";
+export const SVG_PATH = `${baseUrl}img/core-icons.svg`;
+export const ICONIC_PATH = `${baseUrl}img/open-iconic-sprites.svg`;
 
 export const SIDEBAR_SECTIONS = {
   INTRO: "Introduction",
